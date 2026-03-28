57bb3a5c76edd0ca972c125fa86781e00d06bcd1
davidluengos <davidluengosanchez@gmail.com>
2026-03-28 08:42:07 +0100

    cambiada ruta del panel admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
         then: function () {
             Route::middleware(['web', 'auth'])
-                ->prefix('admin')
+                ->prefix('gw-admin')
                 ->name('admin.')
                 ->group(base_path('routes/admin.php'));
         },
